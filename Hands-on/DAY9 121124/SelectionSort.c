747b01469ad23cbffbc1896862b6eed85079a58f
amithhm <153993615+amithhm@users.noreply.github.com>
2024-11-13 04:03:16 +0530

    .

--- Hands-on/DAY9 121124/SelectionSort.c
+++ Hands-on/DAY9 121124/SelectionSort.c
@@ -6,12 +6,15 @@ int main(){
     for(int i=0;i<n-1;i++){
         int c_ele=i;
         for(int j=i+1;j<n;j++){
-            if(a[j]>a[c_ele]){
+            if(a[j]<a[c_ele]){
                 c_ele=j;
             }
         }
         int t=a[i];
         a[i]=a[c_ele];
         a[c_ele]=t;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\t",a[i]);
     }
 }
